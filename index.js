692caec5906b61a6f429c75f4981a53562ab17b5
vmillozza <142731369+vmillozza@users.noreply.github.com>
2024-05-12 16:09:45 +0200

    Primo commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const app = express();
+app.use(express.json());
+app.use(cors());
+// Routes
+app.get("/", (req, res) => {
+  res.send("Server is running..");
+});
+app.post("/api/create-checkout-session", async (req, res) => {
+  const origin = req.get("origin");
+  const lineItems = req.body.map((item) => ({
+    price_data: {
+      currency: "usd",
+      product_data: {
+        name: item.name,
+        images: [item.imageUrl],
+      },
+      unit_amount: Math.round(item.price * 100),
+    },
+    quantity: item.quantity,
+  }));
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ["card"],
+    line_items: lineItems,
+    mode: "payment",
+    success_url: origin + "/success",
+    cancel_url: origin + "/cancel",
+  });
+  res.json({ id: session.id });
+});
+app.listen(8000, () => {
+  console.log("Server started at port 8000");
+});
